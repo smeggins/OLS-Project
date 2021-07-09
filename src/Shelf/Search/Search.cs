ad67c0429c3fdbc8af9c0b809895bead9efc522b
Phillip Chadwick <phillipchadwick@hotmail.com>
2021-07-08 20:04:35 -0700

    adds breakdown of code tasks with dummy code

---
+++ src/Shelf/Search/Search.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLS.src.Shelf.Search
+{
+    class Search
+    {
+    }
+}
